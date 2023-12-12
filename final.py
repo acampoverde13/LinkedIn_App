4b2d1201134ff34323ca86c2e8eaff281ec3a351
Anthony Campoverde <anthonycampoverde@anthonys-mbp.myfiosgateway.com>
2023-12-12 17:14:53 -0500

    app files

--- final.py
+++ final.py
@@ -10,26 +10,23 @@ from sklearn.metrics import accuracy_score
 
 st.markdown("# Welcome to the LinkedIn User Prediction Tool!")
 st.markdown("Please enter values for all of the user features below:")
-st.write("""Use this information below for income & education levels:
-For income:
-    1	Less than $10,000
-	2	10 to under $20,000
-	3	20 to under $30,000
-	4	30 to under $40,000
-	5	40 to under $50,000
-	6	50 to under $75,000
-	7	75 to under $100,000
-	8	100 to under $150,000, OR
-	9	$150,000 or more
-For education:
-    1	Less than high school (Grades 1-8 or no formal schooling)
-	2	High school incomplete (Grades 9-11 or Grade 12 with NO diploma)
-	3	High school graduate (Grade 12 with diploma or GED certificate)
-	4	Some college, no degree (includes some community college)
-	5	Two-year associate degree from a college or university
-	6	Four-year college or university degree/Bachelor’s degree (e.g., BS, BA, AB)
-	7	Some postgraduate or professional schooling, no postgraduate degree (e.g. some graduate school)
-	8	Postgraduate or professional degree (e.g., MA, MS, PhD, MD, JD)
+st.write("""
+         Use this information below for income & education levels:
+         For income:
+            1	Less than $10,000               2	10 to under $20,000
+            3	20 to under $30,000             4	30 to under $40,000
+            5	40 to under $50,000             6	50 to under $75,000
+            7	75 to under $100,000            8	100 to under $150,000, OR
+            9	$150,000 or more
+         For education:
+            1	Less than high school (Grades 1-8 or no formal schooling)
+            2	High school incomplete (Grades 9-11 or Grade 12 with NO diploma)
+            3	High school graduate (Grade 12 with diploma or GED certificate)
+         4	Some college, no degree (includes some community college)
+         5	Two-year associate degree from a college or university
+         6	Four-year college or university degree/Bachelor’s degree (e.g., BS, BA, AB)
+         7	Some postgraduate or professional schooling, no postgraduate degree (e.g. some graduate school)
+         8	Postgraduate or professional degree (e.g., MA, MS, PhD, MD, JD)
          """)
 
 def clean_sm(x):
